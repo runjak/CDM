35ed480aa5a53fc8e08842ce476d751ae67ea9d4
john3jay <dermartini@gmx.de>
2013-12-02 12:45:26 +0100

    HdfsOutput.java Kommentare ergaenzt.

--- eclipse_workspace/Hadoop/src/HdfsOutput.java
+++ eclipse_workspace/Hadoop/src/HdfsOutput.java
@@ -10,27 +10,37 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 /**
  * @author Martin John
- *
+ *gibt die HDFS-Datei /input_data/praktikum/movies.dat auf der Standardausgabe aus
  */
 public class HdfsOutput {
 
 	/**
 	 * @param args
-	 * gibt eine Datei auf der Standardausgabe aus
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		FileSystem fs = null;
 		String fsUri = "hdfs://localhost:8019";
+		
+		//Pfad zur Datei im HDFS
 		String path = "/input_data/praktikum/movies.dat";
 		Path p = new Path(fsUri + path);
+		
 		try {
+		//zum Filesystem verbinden (HDFS muss laufen!!)	
 		fs = FileSystem.get(new URI(fsUri), new Configuration());
+		
+		//Dateigroesse feststellen
 		FileStatus fStat = fs.getFileStatus(p);
 		int size = (int)fStat.getLen();
+		
+		//Datenstrom oeffnen und Datei in Buffer lesen, max Groesse: maxInt Byte
 		FSDataInputStream in = new FSDataInputStream(fs.open(p));
 		byte[] buffer = new byte[size];
 		in.readFully(buffer);
+		
+		//Ausgabe auf System
 		System.out.println(new String(buffer));
 		} catch (Exception e){
 			System.out.println("Exception! ");
